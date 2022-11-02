142535a52c2a86b9b49d14ae62eeae0a10a18ad3
Angelis Apostolos <ap.angelis@gmail.com>
2022-11-02 17:05:40 +0200

    iron session cookie name moved to .env files

--- lib/session.ts
+++ lib/session.ts
@@ -4,7 +4,7 @@ import type { User } from "pages/api/user";
 
 export const sessionOptions: IronSessionOptions = {
   password: process.env.SECRET_COOKIE_PASSWORD as string,
-  cookieName: "iron-session/examples/next.js",
+  cookieName: process.env.SECRET_COOKIE_NAME as string,
   cookieOptions: {
     secure: process.env.NODE_ENV === "production",
   },
