5341a6071157e5e514beb864f1333d4c4bbbc542
Angelis Apostolos <ap.angelis@gmail.com>
2022-11-06 21:03:04 +0200

    Material table in my deposits page

--- pages/mydeposits.tsx
+++ pages/mydeposits.tsx
@@ -72,38 +72,6 @@ interface Data {
   confirmed: boolean;
 }
 
-// function createData(
-//   name: string,
-//   calories: number,
-//   fat: number,
-//   carbs: number,
-//   protein: number,
-// ): Data {
-//   return {
-//     name,
-//     calories,
-//     fat,
-//     carbs,
-//     protein,
-//   };
-// }
-
-// const rows = [
-//   createData('Cupcake', 305, 3.7, 67, 4.3),
-//   createData('Donut', 452, 25.0, 51, 4.9),
-//   createData('Eclair', 262, 16.0, 24, 6.0),
-//   createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-//   createData('Gingerbread', 356, 16.0, 49, 3.9),
-//   createData('Honeycomb', 408, 3.2, 87, 6.5),
-//   createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-//   createData('Jelly Bean', 375, 0.0, 94, 0.0),
-//   createData('KitKat', 518, 26.0, 65, 7.0),
-//   createData('Lollipop', 392, 0.2, 98, 0.0),
-//   createData('Marshmallow', 318, 0, 81, 2.0),
-//   createData('Nougat', 360, 19.0, 9, 37.0),
-//   createData('Oreo', 437, 18.0, 63, 4.0),
-// ];
-
 function descendingComparator<T>(a: T, b: T, orderBy: keyof T) {
   if (b[orderBy] < a[orderBy]) {
     return -1;
@@ -404,7 +372,7 @@ function EnhancedTable(rows: any[]) {
                         {row.title_el}
                       </TableCell>
                       <TableCell align="left">{row.title_en}</TableCell>
-                      <TableCell align="left">{row.confirmed ? "Ναι" : "Όχι" }</TableCell>
+                      <TableCell align="right">{row.confirmed ? "Ναι" : "Όχι" }</TableCell>
                     </TableRow>
                   );
                 })}
