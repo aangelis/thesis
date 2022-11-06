395239fb4028a4361682938a7371937da872c8a9
Angelis Apostolos <ap.angelis@gmail.com>
2022-11-07 00:09:41 +0200

    Display deposit database data

--- pages/deposit/[id].tsx
+++ pages/deposit/[id].tsx
@@ -1,20 +1,70 @@
 import React from "react";
 import { useRouter } from 'next/router'
 import Layout from "components/Layout";
+import { withIronSessionSsr } from "iron-session/next";
+import { sessionOptions } from "lib/session";
+import { User } from "pages/api/user";
+import { InferGetServerSidePropsType } from "next";
 import useUser from "lib/useUser";
+import { PrismaClient } from '@prisma/client'
 
-function deposit() {
-  const { user } = useUser({
-    redirectTo: "/login",
-  });
+// Fetch deposit data
+export const getServerSideProps = withIronSessionSsr(async function ({
+  req,
+  res,
+}) {
+  const user: any = req.session.user;
 
-  const router = useRouter()
-  const { id } = router.query
+  // https://github.com/aralroca/next-translate/issues/694#issuecomment-974717129
+  // Get id property of request data
+  const NextRequestMetaSymbol = Reflect.ownKeys(req).find(key => key.toString() === 'Symbol(NextRequestMeta)');
+  const params = NextRequestMetaSymbol && req[NextRequestMetaSymbol].__NEXT_INIT_QUERY ? req[NextRequestMetaSymbol].__NEXT_INIT_QUERY : undefined
+  const depositId = Number(params.id);
+
+  if (user === undefined) {
+    res.setHeader("location", "/login");
+    res.statusCode = 302;
+    res.end();
+    return {
+      props: {
+        user: { id: null, email: null, username: null, isLoggedIn: false } as User,
+        deposit: {}
+      },
+    };
+  }
+
+  // todo: check if deposit belongs to user in case of not isSuperuser
+  const prisma = new PrismaClient()
+  const deposit: any = await prisma.deposit.findFirst({
+    where: {
+     id: depositId,
+    }
+  })
+
+  return {
+    props : { user, deposit }
+  }
+}, sessionOptions);
+
+
+function DepositPage(
+  { user, deposit }:
+  {
+    user: InferGetServerSidePropsType<typeof getServerSideProps>,
+    deposit: any
+  }
+  ) {
+
+  // const router = useRouter()
+  // const { id } = router.query
   return (
     <Layout>
-    <h1>Deposit info page with id {id}</h1>
+    {/* <h1>Deposit info page with id {id}</h1> */}
+    <h1>Deposit info page with id {deposit.id}</h1>
+
+    <pre>{JSON.stringify(deposit, null, 2)}</pre>
     </Layout>
   )
 }
 
-export default deposit
+export default DepositPage
