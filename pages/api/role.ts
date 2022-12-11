16068fb195664855603816bf298c88c3ef11e3c2
Angelis Apostolos <ap.angelis@gmail.com>
2022-12-11 12:58:16 +0200

    create roles record instead of update when email exists in roles table

--- pages/api/role.ts
+++ pages/api/role.ts
@@ -8,6 +8,12 @@ export default withIronSessionApiRoute(handler, sessionOptions);
 const prisma = new PrismaClient()
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method !== "POST") {
+    // Handle any other HTTP methods
+    res.status(400).json({ message: "Bad HTTP method." });
+    return;
+  }
+
   const user: any = req.session.user;
 
   if (!user?.isAdmin) {
@@ -23,49 +29,49 @@ async function handler(req: NextApiRequest, res: NextApiResponse) {
       );
   }
   
-  if (req.method === "POST") {
-    // Process a POST request
-    const data = await req.body; // role
-    const { id } = data;
-    const validEmail = validateEmail(data.email);
-    console.log(data);
-    console.log(typeof data.is_secretary);
-    
-    if (!data.email ||
-        data.email === "" ||
-        data.email.split('@')[1] !== 'hua.gr' ||
-        validEmail === null ||
-        typeof data.is_admin !== 'boolean' ||
-        typeof data.is_secretary !== 'boolean' ||
-        typeof data.is_librarian !== 'boolean' ||
-        typeof data.is_enabled !== 'boolean') {
-      res.status(400).json({ message: "Invalid data." });
-      return;
-    }
-
-    try {
-      if (data.id) {
-        const role = await prisma.role.update({
-          where: {
-            id,
-          },
-          data
-        })
-        res.json(role);
-      } else {
-        const role = await prisma.role.create({
-          data
-        })
-        res.json(role);
-      }
-      return;
-    } catch (error) {
-      res.status(500).json({ message: (error as Error).message });
-      return;
-    }
-  } else {
-    // Handle any other HTTP methods
-    res.status(400).json({ message: "Bad HTTP method." });
+  // Process a POST request
+  const data = await req.body; // role
+  const { id } = data;
+  const validEmail = validateEmail(data.email);
+  console.log(data);
+  console.log(typeof data.is_secretary);
+  
+  if (!data.email ||
+      data.email === "" ||
+      data.email.split('@')[1] !== 'hua.gr' ||
+      validEmail === null ||
+      typeof data.is_admin !== 'boolean' ||
+      typeof data.is_secretary !== 'boolean' ||
+      typeof data.is_librarian !== 'boolean' ||
+      typeof data.is_active !== 'boolean') {
+    res.status(400).json({ message: "Invalid data." });
     return;
   }
+
+  try {
+    const existingRole = await prisma.role.findUnique({
+      where: { 
+        email: data.email
+      }
+    })
+    if (existingRole) {
+      const role = await prisma.role.update({
+        where: {
+          id: existingRole.id,
+        },
+        data
+      })
+      res.json(role);
+    } else {
+      const role = await prisma.role.create({
+        data
+      })
+      res.json(role);
+    }
+    return;
+  } catch (error) {
+    res.status(500).json({ message: (error as Error).message });
+    return;
+  }
+  
 }
