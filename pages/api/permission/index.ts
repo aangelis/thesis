2089ae5e47d8a9309d24a99c73379914fc92fde0
Angelis Apostolos <ap.angelis@gmail.com>
2022-12-31 15:12:56 +0200

    Permission API endpoint for POST method

---
+++ pages/api/permission/index.ts
@@ -0,0 +1,69 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { PrismaClient } from '@prisma/client'
+import { withIronSessionApiRoute } from "iron-session/next";
+import { sessionOptions } from "lib/session";
+
+export default withIronSessionApiRoute(handler, sessionOptions);
+
+const prisma = new PrismaClient()
+
+async function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method !== "POST") {
+    // Handle any other HTTP methods
+    res.status(400).json({ message: "Bad HTTP method." });
+    return;
+  }
+
+  const user: any = req.session.user;
+
+  if (!user?.isLoggedIn) {
+    res.status(400).json({ message: "Access refused. User not logged in." });
+    return;
+  }
+
+  if (!user?.isSecretary) {
+    res.status(400).json({ message: "Access refused." });
+    return;
+  }
+
+  const validateEmail = (m: string) => {
+    return String(m)
+      .toLowerCase()
+      .match(
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      );
+  }
+  
+  // Process a POST request
+  const data = await req.body; // permission
+
+  if (data.id) {
+    res.status(400).json({ message: "Adding new permission failed. Provided id input." });
+    return;
+  }
+
+  if (!data.submitter_email ||
+    data.submitter_email === "" ||
+    data.submitter_email.split('@')[1] !== 'hua.gr' ||
+    validateEmail(data.submitter_email) === null 
+  ) {
+    res.status(400).json({ message: "Invalid input data." });
+    return;
+  }
+
+  try {
+    const permission = await prisma.permission.create({
+      data: {
+        submitter_email: data.submitter_email,
+        due_to: data.due_to,
+        secretary_id: user.id,
+      }
+    })
+    res.json(permission);
+    return;
+  } catch (error) {
+    res.status(500).json({ message: (error as Error).message });
+    return;
+  }
+  
+}
