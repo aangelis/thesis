6a296537f6e765898f64c96023de7be8c35c0c4d
Angelis Apostolos <ap.angelis@gmail.com>
2023-01-01 12:56:19 +0200

    Role API input data validation using Yup, endpoint split to two files one for PATCH, DELETE and POST method

---
+++ pages/api/role/[id].ts
@@ -0,0 +1,106 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { PrismaClient } from '@prisma/client'
+import { withIronSessionApiRoute } from "iron-session/next";
+import { sessionOptions } from "lib/session";
+import { User } from "pages/api/user";
+import * as yup from 'yup';
+
+export default withIronSessionApiRoute(handler, sessionOptions);
+
+const prisma = new PrismaClient()
+
+async function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method !== "PATCH" && req.method !== "DELETE") {
+    // Handle any other HTTP methods
+    res.status(400).json({ message: "Bad HTTP method." });
+    return;
+  }
+  const user: User = req.session.user!;
+
+  if (!user?.isLoggedIn) {
+    res.status(400).json({ message: "Access refused. User not logged in." });
+    return;
+  }
+
+  if (!user?.isAdmin) {
+    res.status(400).json({ message: "Access refused." });
+    return;
+  }
+
+  const id = parseInt(req.query.id as string);
+
+  if (isNaN(+id)) {
+    res.status(400).json({ message: "Invalid input data." });
+    return;
+  }
+
+  const dbStoredRoleData =
+    (await prisma.role.findUnique({
+      where: {
+        id
+      }
+    }))
+    || 
+    {
+      id: null,
+      secretary_id: null,
+    }
+
+  if (!dbStoredRoleData.id) {
+    res.status(400).json({ message: "Role data not found." });
+    return;
+  }
+
+  if (req.method === 'DELETE') {
+
+    try {
+      const deleteRole = await prisma.role.delete({
+        where: {
+          id
+        },
+      })
+      res.json(deleteRole);
+      return;
+    } catch (error) {
+      res.status(500).json({ message: (error as Error).message });
+      return;
+    }
+
+  }
+
+  if (req.method === 'PATCH') {
+
+    // Process a PATCH request data
+    const data = await req.body; // role
+
+    const roleSchema = yup.object().shape({
+      id: yup.number().integer().required().positive(),
+      email: yup.string().email().required(),
+      is_admin: yup.boolean().required(),
+      is_secretary: yup.boolean().required(),
+      is_librarian: yup.boolean().required(),
+      is_active: yup.boolean().required(),
+    }).noUnknown();
+    
+    if (!(roleSchema.isValidSync(data, { abortEarly: true, strict: true, }))) {
+      res.status(400).json({ message: "Invalid input data." });
+      return;
+    }
+
+    try {
+      const role = await prisma.role.update({
+        where: {
+          id,
+        },
+        data
+      })
+      res.json(role);
+      return;
+    } catch (error) {
+      res.status(500).json({ message: (error as Error).message });
+      return;
+    }
+
+  }
+
+}
---
+++ pages/api/role/index.ts
@@ -0,0 +1,74 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { PrismaClient } from '@prisma/client'
+import { withIronSessionApiRoute } from "iron-session/next";
+import { sessionOptions } from "lib/session";
+import * as yup from 'yup';
+
+export default withIronSessionApiRoute(handler, sessionOptions);
+
+const prisma = new PrismaClient()
+
+async function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method !== "POST") {
+    // Handle any other HTTP methods
+    res.status(400).json({ message: "Bad HTTP method." });
+    return;
+  }
+
+  const user: any = req.session.user;
+
+  if (!user?.isAdmin) {
+    res.status(400).json({ message: "Access refused." });
+    return;
+  }
+
+  // const validateEmail = (m: string) => {
+  //   return String(m)
+  //     .toLowerCase()
+  //     .match(
+  //       /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+  //     );
+  // }
+  
+  // Process a POST request
+  const data = await req.body; // role
+
+  const roleSchema = yup.object().shape({
+    email: yup.string().email().required(),
+    is_admin: yup.boolean().required(),
+    is_secretary: yup.boolean().required(),
+    is_librarian: yup.boolean().required(),
+    is_active: yup.boolean().required(),
+  }).noUnknown();
+  
+  if (!(roleSchema.isValidSync(data, { abortEarly: true, strict: true, }))) {
+    res.status(400).json({ message: "Invalid input data." });
+    return;
+  }
+
+  // const { id } = data;
+  // const validEmail = validateEmail(data.email);
+  // if (!data.email ||
+  //     data.email === "" ||
+  //     data.email.split('@')[1] !== 'hua.gr' ||
+  //     validEmail === null ||
+  //     typeof data.is_admin !== 'boolean' ||
+  //     typeof data.is_secretary !== 'boolean' ||
+  //     typeof data.is_librarian !== 'boolean' ||
+  //     typeof data.is_active !== 'boolean') {
+  //   res.status(400).json({ message: "Invalid data." });
+  //   return;
+  // }
+
+  try {
+    const role = await prisma.role.create({
+      data
+    })
+    res.json(role);
+    return;
+  } catch (error) {
+    res.status(500).json({ message: (error as Error).message });
+    return;
+  }
+  
+}
