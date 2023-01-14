17908063549b58c2d4d111e730bb22e8a061d5c9
Angelis Apostolos <ap.angelis@gmail.com>
2023-01-14 13:00:24 +0200

    migration from SQLite to PostgreSQL

--- prisma/migrations/20230114103249_init/migration.sql
+++ prisma/migrations/20230114103249_init/migration.sql
@@ -1,6 +1,6 @@
 -- CreateTable
 CREATE TABLE "User" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "id" SERIAL NOT NULL,
     "email" TEXT NOT NULL,
     "username" TEXT NOT NULL,
     "first_name" TEXT,
@@ -17,13 +17,15 @@ CREATE TABLE "User" (
     "is_staff" BOOLEAN DEFAULT false,
     "is_active" BOOLEAN DEFAULT true,
     "is_superuser" BOOLEAN DEFAULT false,
-    "last_login" DATETIME,
-    "date_joined" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+    "last_login" TIMESTAMP(3),
+    "date_joined" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "Deposit" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "id" SERIAL NOT NULL,
     "title" TEXT NOT NULL,
     "title_el" TEXT NOT NULL,
     "title_en" TEXT NOT NULL,
@@ -40,7 +42,7 @@ CREATE TABLE "Deposit" (
     "maps" INTEGER NOT NULL DEFAULT 0,
     "drawings" INTEGER NOT NULL DEFAULT 0,
     "confirmed" BOOLEAN NOT NULL DEFAULT false,
-    "confirmed_timestamp" DATETIME,
+    "confirmed_timestamp" TIMESTAMP(3),
     "license" TEXT NOT NULL,
     "comments" TEXT,
     "submitter_id" INTEGER NOT NULL,
@@ -49,27 +51,32 @@ CREATE TABLE "Deposit" (
     "supervisor" TEXT,
     "new_filename" TEXT,
     "original_filename" TEXT,
-    "date_created" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    CONSTRAINT "Deposit_submitter_id_fkey" FOREIGN KEY ("submitter_id") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+    "date_created" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "date_uploaded" TIMESTAMP(3),
+
+    CONSTRAINT "Deposit_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "Role" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "id" SERIAL NOT NULL,
     "email" TEXT NOT NULL,
     "is_admin" BOOLEAN NOT NULL DEFAULT false,
     "is_secretary" BOOLEAN NOT NULL DEFAULT false,
     "is_librarian" BOOLEAN NOT NULL DEFAULT false,
-    "is_active" BOOLEAN NOT NULL DEFAULT false
+    "is_active" BOOLEAN NOT NULL DEFAULT false,
+
+    CONSTRAINT "Role_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "Permission" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "id" SERIAL NOT NULL,
     "submitter_email" TEXT NOT NULL,
-    "due_to" DATETIME NOT NULL,
+    "due_to" TIMESTAMP(3) NOT NULL,
     "secretary_id" INTEGER NOT NULL,
-    CONSTRAINT "Permission_secretary_id_fkey" FOREIGN KEY ("secretary_id") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+
+    CONSTRAINT "Permission_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
@@ -80,3 +87,9 @@ CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "Role_email_key" ON "Role"("email");
+
+-- AddForeignKey
+ALTER TABLE "Deposit" ADD CONSTRAINT "Deposit_submitter_id_fkey" FOREIGN KEY ("submitter_id") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Permission" ADD CONSTRAINT "Permission_secretary_id_fkey" FOREIGN KEY ("secretary_id") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
